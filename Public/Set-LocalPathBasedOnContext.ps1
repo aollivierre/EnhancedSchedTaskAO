4cac94a42edd7632c16927d5cc16f4c0b7c8555f
aollivierre <abdullahollivierre@gmail.com>
2024-10-23 15:12:53 -0700

    minor logging

--- Public/Set-LocalPathBasedOnContext.ps1
+++ Public/Set-LocalPathBasedOnContext.ps1
@@ -1,12 +1,12 @@
 function Set-LocalPathBasedOnContext {
-    Write-EnhancedLog -Message "Checking running context..." -Level "INFO" -ForegroundColor ([System.ConsoleColor]::Cyan)
+    Write-EnhancedLog -Message "Checking running context..." -Level "INFO"
     if (Test-RunningAsSystem) {
-        Write-EnhancedLog -Message "Running as system, setting path to Program Files" -Level "INFO" -ForegroundColor ([System.ConsoleColor]::Yellow)
+        Write-EnhancedLog -Message "Running as system, setting path to Program Files" -Level "INFO"
         # return "$ENV:Programfiles\_MEM"
         return "C:\_MEM"
     }
     else {
-        Write-EnhancedLog -Message "Running as user, setting path to Local AppData" -Level "INFO" -ForegroundColor ([System.ConsoleColor]::Yellow)
+        Write-EnhancedLog -Message "Running as user, setting path to Local AppData" -Level "INFO"
         return "$ENV:LOCALAPPDATA\_MEM"
     }
 }
